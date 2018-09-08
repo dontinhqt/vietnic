6c7106730d61969ccc81ed6ae1feb7acb9407259
Tinh Nguyen <dontinhqt@gmail.com>
2018-09-08 17:38:27 +0700

    change migrate category

--- database/migrations/2017_06_11_015526_create_categories_table.php
+++ database/migrations/2017_06_11_015526_create_categories_table.php
@@ -20,7 +20,7 @@ class CreateCategoriesTable extends Migration
             $table->text('description')->nullable();
             $table->string('cover')->nullable();
             $table->integer('status')->default(0);
-            $table->integer('parent_id')->default(1);
+            $table->integer('parent_id')->default(0);
             $table->timestamps();
         });
     }
